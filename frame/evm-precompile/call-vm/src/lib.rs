fdc7a6755a7fe4e6ac340710aa0f5c6d5e9376ec
wd30130 <wd30130@aliyun.com>
2021-07-08 13:15:11 +0800

    fix(precompile): modified call vm precompile

--- frame/evm-precompile/call-vm/src/lib.rs
+++ frame/evm-precompile/call-vm/src/lib.rs
@@ -4,26 +4,34 @@
 
 #![cfg_attr(not(feature = "std"), no_std)]
 
-extern crate alloc;
-
-use alloc::vec::Vec;
+use sp_std::vec::Vec;
 use core::marker::PhantomData;
 use fp_evm::Precompile;
 use evm::{ExitSucceed, ExitError, Context, executor::PrecompileOutput};
 use pallet_evm::AddressMapping;
-use frame_support::traits::Currency;
-use pallet_contracts::chain_extension::UncheckedFrom;
-use frame_support::sp_runtime::AccountId32;
+//use frame_support::traits::Currency;
+//use pallet_contracts::chain_extension::UncheckedFrom;
+//use frame_support::sp_runtime::AccountId32;
 use frame_system::Config as SysConfig;
+use frame_system::pallet_prelude::*;
 
-pub struct Callwasmc<T: pallet_evm::Config> {
+pub struct CallVm<T: pallet_evm::Config> 
+{
 	_marker: PhantomData<T>,
 }
 
-impl<T> Precompile for Callwasmc<T> where
-	T: pallet_evm::Config + pallet_vm_bridge::pallet::Config,
-	T::AccountId: UncheckedFrom<T::Hash> + AsRef<[u8]> + From<AccountId32>,
-	<<T as pallet_contracts::Config>::Currency as Currency<<T as frame_system::Config>::AccountId>>::Balance: From<u128>,
+pub trait EvmChainExtension<C: SysConfig> {
+	fn call_vm4evm(
+			origin: OriginFor<C>,
+			data: Vec<u8>,
+			target_gas: Option<u64>
+		) -> Result<(Vec<u8>, u64),sp_runtime::DispatchError>;
+}
+
+impl<T> Precompile for CallVm<T> where
+	T: pallet_evm::Config + EvmChainExtension<T>,
+//	T::AccountId: UncheckedFrom<T::Hash> + AsRef<[u8]> + From<AccountId32>,
+//	<<T as pallet_contracts::Config>::Currency as Currency<<T as frame_system::Config>::AccountId>>::Balance: From<u128>,
 	<T as SysConfig>::Origin: From<std::option::Option<<T as SysConfig>::AccountId>>,
 {
 	fn execute(
@@ -34,7 +42,7 @@ impl<T> Precompile for Callwasmc<T> where
 	
 		let origin = T::AddressMapping::into_account_id(context.caller);
 		
-		match pallet_vm_bridge::pallet::Module::<T>::call_wasm4evm(Some(origin).into(), input.iter().cloned().collect(), target_gas) {
+		match T::call_vm4evm(Some(origin).into(), input.iter().cloned().collect(), target_gas) {
 			Ok(ret) => Ok(PrecompileOutput{exit_status:ExitSucceed::Stopped, cost:ret.1, output:ret.0, logs:Vec::new()}),
 			Err(_) => Err(ExitError::Other("call wasmc execution failed".into())),		
 		}		
